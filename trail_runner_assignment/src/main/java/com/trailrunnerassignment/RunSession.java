f608f4036ee2c6c3371a35faa71424f17750b3c2
DreamingJohnny <johnnydreaming@gmail.com>
2025-01-08 10:37:39 +0100

    Added first basic classes

---
+++ trail_runner_assignment/src/main/java/com/trailrunnerassignment/RunSession.java
@@ -0,0 +1,22 @@
+package com.trailrunnerassignment;
+
+public class RunSession {
+	// TODO: This probably needs a constructor for converting meters and such back
+	// and forth
+	private float distance;
+	// TODO: This should probably be replaced with some var that handles amount of
+	// time.
+	private float time;
+	// TODO: This needs to be changed to be a var for handling dates, pretty sure
+	// there is one of those already.
+	private float date;
+	// TODO: All of these will need getters and setters.
+
+	// TODO: Will need at least two constructors, one for if you don't supply the
+	// date
+	public RunSession() {
+
+	}
+
+	//TODO: Add methods below for getting the various stats
+}
---
+++ trail_runner_assignment/src/main/java/com/trailrunnerassignment/User.java
@@ -0,0 +1,17 @@
+package com.trailrunnerassignment;
+
+public class User {
+	
+	//TODO: All of these variables will need getters and setters
+	private float length;
+	private float weight;
+	private int age;
+
+	public User() {
+
+	}
+
+	//TODO: Add getter for returning fitnessScore.
+
+	//TODO: Add methods for getting the other stats
+}
