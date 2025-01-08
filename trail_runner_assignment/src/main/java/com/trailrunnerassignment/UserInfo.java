57d75670c39e2ad76261e8b4fa418bbb9678f900
WilleVirtanen <virtanen.wille@gmail.com>
2025-01-08 14:41:37 +0100

    todos

--- trail_runner_assignment/src/main/java/com/trailrunnerassignment/UserInfo.java
+++ trail_runner_assignment/src/main/java/com/trailrunnerassignment/UserInfo.java
@@ -7,11 +7,44 @@ public class UserInfo {
 	private float weight;
 	private int age;
 
-	public UserInfo() {
+	/*public float getLength() {
+		return length;		
+	}*/
 
-	}
+	/*public void setLength(float length) {
+		this.length = length;
+	}*/
 
+	/*public float getWeight() {
+		return weight;
+	}*/
+
+	/*public void setWeight(float weight) {
+		this.weight = weight;
+	}*/
+
+	/*public int getAge() {
+		return age;
+	}*/
+
+	/*public void setAge(int age) {
+		this.age = age;
+	}*/
+
+	/*public UserInfo(float length, float weight,int age) {
+		this.length=length;
+		this.weight=weight;
+		this.age=age;
+
+	}*/
+
+	/*public float getFitnessScore()
+	{
+		//value=cs+(d+v/p)-t/2, return value
+		return 0;
+	}*/
 	//TODO: Add getter for returning fitnessScore.
 
 	//TODO: Add methods for getting the other stats
+	
 }
