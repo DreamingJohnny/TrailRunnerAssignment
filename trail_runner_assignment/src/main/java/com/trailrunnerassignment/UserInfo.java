dc3a9f977ec69a3a3bfcc07701fc533f1d62377d
DreamingJohnny <johnnydreaming@gmail.com>
2025-01-08 19:54:32 +0100

    Adding UserInfo tests

--- trail_runner_assignment/src/main/java/com/trailrunnerassignment/UserInfo.java
+++ trail_runner_assignment/src/main/java/com/trailrunnerassignment/UserInfo.java
@@ -2,50 +2,8 @@ package com.trailrunnerassignment;
 
 public class UserInfo {
 	
-	//TODO: All of these variables will need getters and setters
 	private float length;
 	private float weight;
 	private int age;
 	
-	public float getFitnessScore() {
-
-		return -1;
-	}
-
-	/*public float getLength() {
-		return length;		
-	}*/
-
-	/*public void setLength(float length) {
-		this.length = length;
-	}*/
-
-	/*public float getWeight() {
-		return weight;
-	}*/
-
-	/*public void setWeight(float weight) {
-		this.weight = weight;
-	}*/
-
-	/*public int getAge() {
-		return age;
-	}*/
-
-	/*public void setAge(int age) {
-		this.age = age;
-	}*/
-
-	/*public UserInfo(float length, float weight,int age) {
-		this.length=length;
-		this.weight=weight;
-		this.age=age;
-
-	}*/
-
-	/*public float getFitnessScore()
-	{
-		//value=cs+(d+v/p)-t/2, return value
-		return 0;
-	}*/	
 }
--- trail_runner_assignment/src/test/java/com/trailrunnerassignment/UserInfoTest.java
+++ trail_runner_assignment/src/test/java/com/trailrunnerassignment/UserInfoTest.java
@@ -1,37 +1,87 @@
 package com.trailrunnerassignment;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UserInfoTest {
 
-	/*
-	 * Går det att skapa en användare?
-	 * Har den ett fitness score?
-	 * Har den dagar sen senaste löpturen?
-	 * Kan fitness score vara lägre än noll?
-	 * Innan första löprundan är fitness score noll?
-	 * Innan första löprundan, är dagar sen senaste löpturen noll?
-	 */
-
 	public UserInfo userInfo;
 
+	private float length;
+	private float weight;
+	private float age;
+
+	public RunSession runSession;
+
+	private float distance;
+	private float time;
+	private float date;
+
+	private float expectedValue;
+
+	/*
+	 * Går det att skapa en användare med korrekt uppgifter?
+	 */
 	@BeforeEach
 	public void setup() {
-		userInfo = new UserInfo();
-	}
-
-	@Test
-	public void canCreateUserInfo() {
-		userInfo = null;
+		userInfo = new UserInfo(length, weight, age);
 
 		assertTrue(userInfo != null);
 	}
 
+	/* Går det att få ut ett korrekt fitness score? */
 	@Test
-	public void canGetFitnessScore() {
-		assertTrue(userInfo.getFitnessScore() != -1);
+	public void getCorrectFitnessScore() {
+		expectedValue = 10;
+
+		assertEquals(expectedValue, userInfo.getFitnessScore());
 	}
 
+	/* Kan fitness score vara lägre än noll? */
+	@Test
+	public void fitnessScoreNeverBelowZero() {
+		expectedValue = 0;
+
+		assertEquals(expectedValue, userInfo.getFitnessScore());
+	}
+
+	/* Innan första löprundan är fitness score noll? */
+	@Test
+	public void fitnessScoreZeroAtStart() {
+		expectedValue = 0;
+
+		assumeTrue(userInfo.getRunSessionsAmount() == 0,
+				"Test cannot run if the user has one or more running sessions logged already.");
+
+		assertEquals(expectedValue, userInfo.getFitnessScore());
+	}
+
+	/* Innan första löprundan, är dagar sen senaste löpturen noll? */
+	@Test
+	public void daysSinceLastRunZeroAtStart() {
+		expectedValue = 0;
+
+		assumeTrue(userInfo.getRunSessionsAmount() == 0,
+				"Test cannot run if the user has one or more running sessions logged already.");
+
+		assertEquals(expectedValue, userInfo.daysSinceLastRun());
+	}
+
+	/* Är dagar sen senaste löpturen korrekt? */
+	@Test
+	public void setsDateSinceLastRunCorrectly() {
+
+		expectedValue = 7;
+
+		// So, here I'll want to begin by sending in a run with a date then, and then
+		// check so that it sets it correctly then
+		userInfo.addRunSession(new RunSession(distance, time, date));
+
+		assertEquals(expectedValue, userInfo.daysSinceLastRun());
+	}
 }
