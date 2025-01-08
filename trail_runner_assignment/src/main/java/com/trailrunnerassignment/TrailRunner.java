eb887a141c1d3288b08a23e332d781e9a1ba2b1c
DreamingJohnny <johnnydreaming@gmail.com>
2025-01-08 13:52:11 +0100

    Added comments for tests we should add

--- trail_runner_assignment/src/main/java/com/trailrunnerassignment/TrailRunner.java
+++ trail_runner_assignment/src/main/java/com/trailrunnerassignment/TrailRunner.java
@@ -3,5 +3,5 @@ package com.trailrunnerassignment;
 public class TrailRunner {
 	
 	//So, this will be the "container" for the whole app, that will contain a list of the various runSessions, one user etc.
-	//Since there will only be one of these, I'm thinking that this should be static in some way?
+	//Since there will only be one of these, I'm thinking that this should be static or something similar?
 }
---
+++ trail_runner_assignment/src/test/java/com/trailrunnerassignment/TrailRunnerTest.java
@@ -0,0 +1,8 @@
+package com.trailrunnerassignment;
+
+public class TrailRunnerTest {
+	/*
+	 * testa så att det inte går att få flera av samma identifikationsnummer?
+	 * Får användaren ett fitness score i slutet av varje avslutad löprunda?
+	 */
+}
